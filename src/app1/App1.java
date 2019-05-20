bec487d9856003f69957dcdfb5fad77a223de778
Bruna <bruna.lima@zup.com.br>
2019-05-20 13:52:34 -0300

    Serve para troca de valores

---
+++ src/app1/App1.java
@@ -0,0 +1,50 @@
+package app1;
+
+import java.util.Scanner;
+
+public class App1 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		int valorA = 10;
+		int valorB = 2;
+		int troca;
+		
+		System.out.println("Valor de A =" + valorA);
+		System.out.println(" Valor de B =" + valorB);
+		System.out.println("");
+		
+		troca = valorA;
+		valorA = valorB;
+		valorB = troca;
+		
+		System.out.println("Valor de A =" + valorA);
+		System.out.println(" Valor de B =" + valorB);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+	    
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
